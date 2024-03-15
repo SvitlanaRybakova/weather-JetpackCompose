9763d4d586a633d851b677e78dd76565ff2d90d1
Svitlana <rsv0011@gmail.com>
2024-03-15 13:18:52 +0100

    feat: implement ListItem layout

--- app/src/main/java/com/example/weather/screens/MainCard.kt
+++ app/src/main/java/com/example/weather/screens/MainCard.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -146,7 +148,8 @@ fun TabLayout() {
                     Modifier.pagerTabIndicatorOffset(pagerState, position)
                 )
             },
-            backgroundColor = BlueLight
+            backgroundColor = BlueLight,
+            contentColor = Color.White
         ) {
             tabList.forEachIndexed { index, text ->
                 Tab(
@@ -157,7 +160,7 @@ fun TabLayout() {
                         }
                     },
                     text = {
-                        Text(text = text)
+                        Text(text = text, style = TextStyle(color = Color.White))
                     }
                 )
             }
@@ -167,6 +170,13 @@ fun TabLayout() {
             state = pagerState,
             modifier = Modifier.weight(1.0f)
         ) { index ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(15) {
+                    ListItem()
+                }
+            }
         }
     }
 }
